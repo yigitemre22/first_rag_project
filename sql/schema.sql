43de8545a4d52c7b7353a48ba3cb0b239af34a1e
Yiğit Emre <emre2003yoruk@gmail.com>
2026-07-12 01:29:41 +0300

    Initial project setup

---
+++ sql/schema.sql
@@ -0,0 +1,9 @@
+create extension if not exist on vector;
+
+create table if not exist documents(
+    id SERIAL primary key,
+    filename TEXT not null,
+    chunk TEXT not null,
+    embedding VECTOR(384),
+    created_at TIMESTAP default CURRENT_TIMESTAP
+);
