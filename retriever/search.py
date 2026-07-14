17d8446a070bc01ed1dc26af7b3ccbe824854efc
Yiğit Emre <emre2003yoruk@gmail.com>
2026-07-15 01:39:01 +0300

    implement semantic search retriever with pgvector

---
+++ retriever/search.py
@@ -0,0 +1,28 @@
+from embeddings.embedding import generate_embeddings
+from database.db import get_connection
+
+def search_documents(
+        query:str,
+        top_k:int=5
+        ) ->list[tuple[str,str]]:
+    
+    query_embedding=str(generate_embeddings([query])[0])
+    with get_connection() as conn:
+        with conn.cursor()  as cur:
+            cur.execute(
+                "select filename, " \
+                "chunk, " \
+                "embedding <=> %s::vector as distance " \
+                "from documents " \
+                "order by distance " \
+                "limit %s",
+                (query_embedding,top_k)
+            )
+            results=cur.fetchall()
+            return results
+        
+if __name__=="__main__":
+    results=search_documents("sanal bellek nedir")
+
+    for result in results:
+        print(result)
